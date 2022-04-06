17b92795f4af961d09af49031f54cb0778bb4b8b
Lev Shuster <shusterl@carleton.edu>
2022-04-05 20:54:41 -0500

    drafted a reverse function

--- lab0-handout/queue.c
+++ lab0-handout/queue.c
@@ -39,31 +39,31 @@ Queue *q_new()
 void q_free(Queue *q)
 {
     // What if q is NULL?  **[erhaps do opisite]
-    if(q == NULL){
-      return;
+    if(q != NULL){
+
+      // TODO free the queue nodes
+      /* You'll want to loop through the list nodes until the next pointer is NULL,
+      * starting at the head, freeing each node and its string. 
+      * Account for an empty list (head is NULL). */
+      struct node *tempHead;
+      // Node tempHead; //try if 49 not working
+
+      while(q->head != NULL){
+        tempHead = q->head;
+        q->head = q->head->next;
+        free(tempHead->value);
+        free(tempHead);
+        // free(q->tail);
+        // free(q->size);
+      }
+
+      // q_free(q->tail);
+      // free(q->head);
+
+
+      // Freeing queue structure itself
+      free(q);
     }
-    // TODO free the queue nodes
-    /* You'll want to loop through the list nodes until the next pointer is NULL,
-     * starting at the head, freeing each node and its string. 
-     * Account for an empty list (head is NULL). */
-    struct node *tempHead;
-    // Node tempHead; //try if 49 not working
-
-    while(q->head != NULL){
-      tempHead = q->head;
-      q->head = q->head->next;
-      free(tempHead->value);
-      free(tempHead);
-      // free(q->tail);
-      // free(q->size);
-    }
-
-    // q_free(q->tail);
-    // free(q->head);
-
-
-    // Freeing queue structure itself
-    free(q);
 }
 
 /*
@@ -91,7 +91,7 @@ bool q_insert_head(Queue *q, char *s)
     // See the C Tutor example linked from page 4 of the writeup!
     // If this second malloc call returns NULL, you need to free newh before returning
     newh->value = (char*) malloc(strlen(s) + 1);
-    if(newh == NULL){
+    if(newh->value == NULL){
       free(newh);
       return false;
     }
@@ -138,7 +138,8 @@ bool q_insert_tail(Queue *q, char *s)
     // See the C Tutor example linked from page 4 of the writeup!
     // If this second malloc call returns NULL, you need to free newh before returning
     newh->value = (char*) malloc(strlen(s) + 1);
-    if(newh == NULL){
+    if(newh->value == NULL){
+      printf("I failed here");
       free(newh);
       return false;
     }
@@ -153,7 +154,7 @@ bool q_insert_tail(Queue *q, char *s)
       q->tail->next = newh;
       q->tail = newh;
     }
-    // newh->next = NULL;
+    newh->next = NULL;
 
     q->size += 1;
 
@@ -188,18 +189,20 @@ bool q_remove_head(Queue *q, char *sp, long bufsize)
       // to the last index of sp
 
       // update q->head to remove the current head from the queue
-      printf("hi round thing");
-      Node *eric;
-      eric = q->head;
+      // printf("hi round thing");
+      Node *temp;
+      //struct node *temp;
+
+      temp = q->head;
       q->head = q->head->next;
       q->size -= 1;
       // if the last list element was removed, the tail might need updating
       if(q->size == 0){
         q->tail = NULL;
       }
+      free(temp->value);
+      free(temp);
     }
-    free(eric->value);
-    free(eric);
     return true;
 }
 
@@ -209,8 +212,12 @@ bool q_remove_head(Queue *q, char *sp, long bufsize)
  */
 int q_size(Queue *q)
 {
+    if (q == NULL){
+      return 0;
+    }else{
+      return q->size;
+    }
     // What if q is NULL?
-    return q->size;
 }
 
 /*
@@ -220,7 +227,42 @@ int q_size(Queue *q)
   (e.g., by calling q_insert_head, q_insert_tail, or q_remove_head).
   It should rearrange the existing ones.
  */
+
+// Node* q_reverse_helper(Node* currentNode, Queue *q)
+// {
+//   char *temp = currentNode->value;
+//   if(currentNode == q->tail){
+//     currentNode->value = q->head->value;
+//     q->head->value = temp;
+//     printf("flipped %s with %s", q->head->value, currentNode->value);
+//     return q->head->next;
+//   }else{
+//     Node* front_node = q_reverse_helper(currentNode->next, q);
+//     currentNode->value = front_node->value;
+//     front_node->value = temp;
+//     printf("flipped %s with %s", currentNode->value, front_node->value);
+
+//     return front_node->next;
+//   }
+// }
+
 void q_reverse(Queue *q)
 {
-    /* You need to write the code for this function */
-}
+    // TODO check if q is NULL or empty
+    // if(q != NULL && q->size != 0){ // may need to change behavor of sp
+    //   q_reverse_helper(q->head, q);
+    // }
+}    
+
+// int main(void){
+//   Queue *test = q_new();
+//   q_insert_head(test, "a");
+//   q_insert_tail(test, "b");
+//   q_insert_tail(test, "c");
+//   q_insert_tail(test, "d");
+//   q_size(test);
+//   q_reverse(test);
+//   char* temp = malloc(sizeof("a"));
+//   q_remove_head(test, temp, 2);
+//   q_free(test);
+// }
