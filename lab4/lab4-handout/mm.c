df91d5b4945336080cfda54fd309f8b8a2d1a2ad
Lev Shuster <shusterl@carleton.edu>
2022-05-22 12:47:50 -0500

    debugging explicit free lists

--- lab4/lab4-handout/mm.c
+++ lab4/lab4-handout/mm.c
@@ -77,11 +77,17 @@
 #define NEXT_BLKP(bp)  (PADD(bp, GET_SIZE(HDRP(bp))))
 #define PREV_BLKP(bp)  (PSUB(bp, GET_SIZE((PSUB(bp, DSIZE)))))
 
-
 /* Given block ptr bp, return the allocation status of the block preceding (in memory order) */
 #define PREV_ALLOC(bp) (GET_ALLOC(HDRP(PREV_BLKP(bp)))); // TODO test to make sure works as exspected
-/* Given the two pointers of two neighboring unalloced blocks, merge them*/
-#define COALESCE(h1, h2) (PUT(h1, GET(h1) + GET(h2))) //double check that first arg should be h1
+
+/* macros for explicit linked lists */
+//names flipped without checking all impacts
+#define PTR_NEXT_PTR(bp)  ((size_t *) bp) // Returns the pointer to the number that functions as the pointer to the bp of the previous value
+#define PTR_PREV_PTR(bp)  ((size_t *) PADD(bp, WSIZE)) // Returns the pointer to the number that functions as the pointer to the bp of the next value
+
+#define PREV_PTR(bp)  (*PTR_PREV_PTR(bp)) // Returns the pointer to the previous bp
+#define NEXT_PTR(bp)  (*PTR_NEXT_PTR(bp)) // Returns the pointer to the next bp
+
 
 /* Global variables */
 
@@ -93,6 +99,7 @@ static void *heap_start = NULL;
 static bool check_heap(int lineno);
 static void print_heap();
 static void print_block(void *bp);
+static void print_linked_list();
 static bool check_block(int lineno, void *bp);
 static void *extend_heap(size_t size);
 static void *find_fit(size_t asize);
@@ -100,7 +107,7 @@ static void *coalesce(void *bp);
 static void place(void *bp, size_t asize);
 static size_t max(size_t x, size_t y);
 
-/*
+/* UPDATED FOR EXPLICIT LINKED LIST
  * mm_init -- <What does this function do?>
  * <What are the function's arguments?>
  * <What is the function's return value?>
@@ -111,8 +118,9 @@ int mm_init(void) {
     if ((heap_start = mem_sbrk(4 * WSIZE)) == NULL)
         return -1;
 
-    PUT(heap_start, 0);                        /* alignment padding */
-    PUT(PADD(heap_start, WSIZE), PACK(OVERHEAD, 1));  /* prologue header */
+    PUT(heap_start, (size_t) PADD(heap_start, MIN_B_SIZE));                        /* alignment padding set to the pointer to the first availble block (padding+prologue+header of the first block)*/
+    
+    PUT(PADD(heap_start, WSIZE), PACK(OVERHEAD, 1));  /* prologue header */ // sets header after empty space
     PUT(PADD(heap_start, DSIZE), PACK(OVERHEAD, 1));  /* prologue footer */
     PUT(PADD(heap_start, WSIZE + DSIZE), PACK(0, 1));   /* epilogue header */
 
@@ -122,16 +130,20 @@ int mm_init(void) {
     if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
         return -1;
 
+    PUT(PTR_NEXT_PTR(NEXT_PTR(PSUB(heap_start, DSIZE))), (size_t) 0); // put 0 @ the first free block 'next' pointer
+    PUT(PTR_PREV_PTR(NEXT_PTR(PSUB(heap_start, DSIZE))), (size_t) 0); // put 0 @ the first free block 'next' pointer
+
     return 0;
 }
 
-/*
+/* UPDATED FOR EXPLICIT LINKED LIST
  * mm_malloc -- <What does this function do?>
  * <What are the function's arguments?>
  * <What is the function's return value?>
  * <Are there any preconditions or postconditions?>
  */
 void *mm_malloc(size_t size) {
+    printf("\n\nstarting to malloc\n");
     size_t asize;      /* adjusted block size */
     size_t extendsize; /* amount to extend heap if no fit */
     char *bp;
@@ -160,6 +172,8 @@ void *mm_malloc(size_t size) {
         return NULL;
 
     place(bp, asize);
+    printf("\n\nfinished to malloc\n");
+
     return bp;
 }
 
@@ -172,7 +186,7 @@ void *mm_malloc(size_t size) {
  * from malloc 
  */
 void mm_free(void *bp) {
-    //printf("\n\n\n starting to free \n");
+    printf("\nstarting to free\n");
     // Calculate what the header/footer would be unallocated:
     size_t newhead = GET_SIZE(HDRP(bp)); // size_t is meant to functinoally be an unsigned long
     newhead = PACK(newhead, 0); // masking: ... & (~0xf)   //TODO turn into bitwise operation or find out about decrement operator (--)
@@ -180,66 +194,126 @@ void mm_free(void *bp) {
     *HDRP(bp) = newhead; // TODO test to make sure this updates header correctly
     // Make the last bit of the footer 0.
     *FTRP(bp) = newhead; // variable reuse!
-    //printf("heap at the end of free\n");
-    //printf("the allocation state of this block is: %i \n\n", (int) GET_ALLOC(HDRP(bp)));
-    // print_heap();
-    // check_heap(__LINE__);
+
+    size_t* padding = (size_t*) PSUB(heap_start, DSIZE);
+    size_t* second = (size_t*) NEXT_PTR(padding);
+
+    PUT(PTR_PREV_PTR(second), (size_t) bp); // set previous_ll of first item in linked list to be bp
+    PUT(PTR_NEXT_PTR(bp), (size_t) second); // set bp ll_next to current first item
+    PUT(PTR_PREV_PTR(bp), (size_t) 0); // set bp previous to equal 0 cast as something
+    PUT(padding, (size_t) bp); // set current first item in linked list to be the next value of bp
+   
+    printf("\nfinished to free\n");
 
 }
 
 /* The remaining routines are internal helper routines */
 
 
-/*
- * place -- Place block of asize bytes at start of free block bp
+/* UPDATED FOR LINKED LIST
+ * place -- Place block of asize bytes at start of free block bp and remove block from the explicit free linked list
  *          and <How are you handling splitting?>
  * Takes a pointer to a free block and the size of block to place inside it
  * Returns nothing
  * <Are there any preconditions or postconditions?>
  */
 static void place(void *bp, size_t asize) { // 'asize' is just the size of the payload + head/foot + padding O
-    // calc block size (total w/ byte-alignment)
-    // printf("\n\ntrying to place\n\n");
-    //      done by dividing asize by DSIZE then rounding up and multiplying by 16
+    printf("\n\nstarting to place\n");
+    print_linked_list();
+        
     size_t unalloc_size = GET_SIZE(HDRP(bp)) - asize; // size of unallocated remainder - used for splitting
-    /* TESTING */
-    // printf("Current total unallocated: %i \n", (int) GET_SIZE(HDRP(bp)));
-    // printf("Allocating %ld bytes \n", asize);
-    // printf("allocated %ld bytes, %ld bytes remain \n", asize, unalloc_size);
-
-    // //      option 2 : size_t size = asize + (asize % 16) + 16;
-
-    // // set header/footer to be size++ (total size + allocation bit)
-    size_t header_value = PACK(asize, 1); //double check that this should be size_t// masking: ... & ((~0)-1)   
-    //TODO turn into bitwise operation or find out about decrement operator (--)    
-        //  header_value = size + 1;
-
-    // // Setting Header/Footer values for 
+    size_t header_value = PACK(asize, 1); //double check that this should be size_t// masking: ... & ((~0)-1) 
+    size_t* prev = (size_t*) PREV_PTR(bp);
+    size_t* next = (size_t*) NEXT_PTR(bp);  
+    // Setting Header/Footer values for 
     PUT(HDRP(bp), header_value); // Update header
-    // //      find location of footer by decrementing size by 8
     PUT(FTRP(bp), header_value);
-    // PUT(PADD(bp, (size_t) PSUB(asize, WSIZE)), header_value); // Update footer, translation : set the value @ (bp + (size-8)) to header_value
+    // removing the block from the linked list:
 
     // // SPLITTING:
     if (unalloc_size > 0) {
-        //old code: size_t next_head = (size_t) PADD(HDRP(bp), asize);
-        void* next = NEXT_BLKP(bp);
-        PUT(HDRP(next), unalloc_size); // Update header
-        PUT(FTRP(next), unalloc_size); // Update footer
+        printf("decided that splitting is necessary\n");
+        size_t* unalloc_blk = (size_t*) NEXT_BLKP(bp);
+        PUT(HDRP(unalloc_blk), unalloc_size); // Update header
+        PUT(FTRP(unalloc_blk), unalloc_size); // Update footer
+        
+        // blk points outwards
+        PUT(PTR_PREV_PTR(unalloc_blk), (size_t) prev);
+        PUT(PTR_NEXT_PTR(unalloc_blk), (size_t) next);
+        
+        // makes neibors in linked list point to unallocated portion of the newly split block
+        if (prev){ // if previous is not null update previous value
+            PUT(PTR_NEXT_PTR(prev), (size_t) unalloc_blk); // set the 'next' pointer of the previous block
+        }
+        if (next) { // if next can be set (non-NULL)
+            PUT(PTR_PREV_PTR(next), (size_t) unalloc_blk); // set the 'prev' pointer of the next block
+        }
+    } else {
+        printf("decided that splitting is not necessary\n");
+        if (next == NULL) {
+            PUT(PTR_NEXT_PTR(prev), (size_t) 0);
+        } if (prev == NULL) {
+            PUT(PTR_PREV_PTR(next), (size_t) 0);
+
+            
+        } else if ((prev != NULL) && (next != NULL)) {
+            PUT(PTR_PREV_PTR(next), (size_t)prev);
+            PUT(PTR_NEXT_PTR(prev), (size_t)next);
+        }
+
+        /* orignial thinking
+        void* unalloc_blk = NEXT_BLKP(bp);
+        PUT(PREV_PTR(unalloc_blk), (size_t) unalloc_blk);
+        PUT(NEXT_PTR(unalloc_blk), (size_t) unalloc_blk);
+        // EXTERNAL:
+        PUT(PTR_NEXT_PTR(PREV_PTR(unalloc_blk)), (size_t) unalloc_blk);
+        PUT(PTR_PREV_PTR(NEXT_PTR(unalloc_blk)), (size_t) unalloc_blk);
+        */
     }
+    print_linked_list();
+    printf("finished place\n");
 
-    // PUT(PADD(next_head, (size_t) PSUB(unalloc_size, WSIZE)), unalloc_size); // Update footer
-    // printf("Bytes split: %ld \n", GET_SIZE(next_head));
 
-    // setting header/footer
+}
 
-    // orignal code from lab assigment 
-    // currently does no splitting, just allocates the entire free block
-    // PUT(HDRP(bp), PACK(GET_SIZE(HDRP(bp)), 1));
-    // PUT(FTRP(bp), PACK(GET_SIZE(HDRP(bp)), 1));
-    //printf("\n]n heap at the end of place");
-    //print_heap();
+static void linked_list_coalesce_helper(void *bp) {
+    printf("\n\nstarting to coalesce the linked list\n");
+
+
+    void* next = NEXT_BLKP(bp); // next block, physically
+    void* prev = PREV_BLKP(bp); // previous block, physically
+    void* previous_header = HDRP(prev); // Make a pointer to the previous header
+    void* next_header = HDRP(next); // Make a pointer to the next header
     
+    if ( (GET(previous_header) && !GET_ALLOC(previous_header)) && (GET(next_header) && !GET_ALLOC(next_header)) ) { // if: (previous exists and unalloc) and (next exists and unalloc)
+            //remove the previously empty block affter the newly freed block from the linked list
+            void* ll_next = (void*) NEXT_PTR(next); // next linked list item
+
+            /* what's going on here????????????????? */
+            PUT(PTR_NEXT_PTR(PREV_PTR(ll_next)), NEXT_PTR(ll_next)); // set the 'next' pointer of the left block to  
+            PUT(PTR_PREV_PTR(NEXT_PTR(ll_next)), PREV_PTR(ll_next)); // set the 'prev' pointer of the right block to
+
+    } else if (GET(next_header) && !GET_ALLOC(next_header)) { // move previous and next to the start of the newly bigger block and change pointers of the ll next and ll previous block
+        void* ll_next = (void*) NEXT_PTR(next); // next linked list item
+        void* ll_prev = (void*) PREV_PTR(next); // prev linked list item
+        // set the next_ptr values of the newly freed block to the values of the previously freed block after it
+        PUT(PTR_NEXT_PTR(bp), NEXT_PTR(next)); 
+        PUT(PTR_PREV_PTR(bp), PREV_PTR(next)); 
+        
+        // set the neighbors of the previously freed block that follow the newly freed block to point to the newly freed block that will now be coalesed with the existing free block
+        PUT(PTR_PREV_PTR(ll_next), (size_t) bp); // put [a pointer to the bigger block] @ [the 'previous' field of the next block
+        PUT(PTR_NEXT_PTR(ll_prev), (size_t) bp); // put [a pointer to the bigger block] @ [the 'previous' field of the next block
+    
+    } else { //only add current block to the lined list
+        size_t* start = (size_t*) PSUB(heap_start, DSIZE);
+        size_t* second_item = (size_t*) *start; 
+        PUT(PTR_NEXT_PTR(bp), (size_t) second_item); // set bp's next item to be the second item
+        PUT(PTR_PREV_PTR(second_item), (size_t) bp); // set the second item's previous value to bp
+        PUT(PTR_PREV_PTR(bp), (size_t) 0);        // set bp's previous value to NULL
+        PUT(start, (size_t) bp);                     // put bp as start's value  
+    }
+    printf("\n\nfinished coaliscing the linked list\n");
+
 }
 
 /*
@@ -250,57 +324,53 @@ static void place(void *bp, size_t asize) { // 'asize' is just the size of the p
  * * bp must point to a block that is already free
  */
 static void *coalesce(void *bp) {
+    // coalesce the linked list without effecting the blocks:
+    linked_list_coalesce_helper(bp); // updates the linked-list pieces
+    printf("\ncoalescing (physical)\n");
 
-    // void* prev = PREV_BLKP(bp); // bp but for prev block
-    // void* next = NEXT_BLKP(bp); // bp but for next block
-    // int prevfree = GET_ALLOC(HDRP(prev)); // 0 if free, 1 if not
-    // int nextfree = GET_ALLOC(HDRP(next)); // 0 if free, 1 if not
-    // size_t totalsize = GET_SIZE(HDRP(bp)); // total size 
-
-    // if (!prevfree && nextfree) { // coalescing left only
-    //     totalsize = totalsize + GET_SIZE(HDRP(prev)); // total size += prev's size
-    //     bp = prev; // set bp to prev's bp
-    // } else if (prevfree && !nextfree) { // coalescing right only
-    //     totalsize = totalsize + GET_SIZE(HDRP(next)); // total size += next's size
-    //     // Don't need to change the bp, as it is already at the leftmost
-    // } else if (!prevfree && !nextfree) { // coalescing both
-    //     totalsize = totalsize + (GET_SIZE(HDRP(prev)) + GET_SIZE(HDRP(next))); // total size increased for whole block-set
-    //     bp = prev; // set bp to prev's bp
-    // }
-    // PUT(HDRP(bp), totalsize); // set header of "bp" (whatever we set bp to)
-    // PUT(FTRP(bp), totalsize); // set footer of "bp" (whatever we set bp to)
-    // return bp;
-    size_t retval = (size_t) bp;
+    void* retval = bp;
     //CHECKING PREVIOUS:
-    size_t previous_header = (size_t) HDRP(PREV_BLKP(bp)); // Make a pointer to the previous header
+    void* previous_header = HDRP(PREV_BLKP(bp)); // Make a pointer to the previous header
     if (!GET_ALLOC(previous_header)) { // if previous is unallocated:
-        retval = (size_t) PREV_BLKP(bp); // set the retval to the previous block pointer
+        retval = PREV_BLKP(bp); // set the retval to the previous block pointer
         size_t totalsize = GET_SIZE(previous_header) + GET_SIZE(HDRP(bp));
         PUT(HDRP(retval), totalsize);
         PUT(FTRP(retval), totalsize);
     } 
     // CHECKING NEXT:
-    size_t next_header = (size_t) HDRP(NEXT_BLKP(bp)); // Make a pointer to the next header
+    void* next_header = HDRP(NEXT_BLKP(bp)); // Make a pointer to the next header
     if (!GET_ALLOC(next_header)) { // if next is unallocated:
         // we don't need to change the retval here
         size_t totalsize = GET_SIZE(next_header) + GET_SIZE(HDRP(bp));
         PUT(HDRP(retval), totalsize);
         PUT(FTRP(retval), totalsize);
     }
-    return (void*) retval;
+    printf("finished coalescing (physical)\n");
+    return retval;
 }
 
 
-/*
+
+/* MODIFIED FOR LINKED LIST
  * find_fit - Find a fit for a block with asize bytes
  */
 static void *find_fit(size_t asize) {
+    printf("\nfinding fit\n");
     /* search from the start of the free list to the end */
-    for (char *cur_block = heap_start; GET_SIZE(HDRP(cur_block)) > 0; cur_block = NEXT_BLKP(cur_block)) {
-        if (!GET_ALLOC(HDRP(cur_block)) && (asize <= GET_SIZE(HDRP(cur_block))))
-            return cur_block;
-    }
+    // for (char *cur_block = heap_start; GET_SIZE(HDRP(cur_block)) > 0; cur_block = NEXT_BLKP(cur_block)) {
+    //     if (!GET_ALLOC(HDRP(cur_block)) && (asize <= GET_SIZE(HDRP(cur_block))))
+    //         return cur_block;
+    // }
 
+    for (char *cur_block = (char*) NEXT_PTR(PSUB(heap_start, DSIZE)); PTR_NEXT_PTR(cur_block) != NULL; cur_block = (char *) NEXT_PTR(cur_block)) {
+        // heap_start is set to the blk ptr of the prologue, so we do (heap_start-16) to get to the padding at the front
+        if (asize <= GET_SIZE(HDRP(cur_block))) { // Since this is an explicit linked list containing ONLY free items, checking for freedom would be redundant
+            printf("fit found: ");
+            print_block(cur_block);
+            return cur_block;
+        }
+    }
+    printf("fit not found...\n");
     return NULL;  /* no fit found */
 }
 
@@ -308,6 +378,8 @@ static void *find_fit(size_t asize) {
  * extend_heap - Extend heap with free block and return its block pointer
  */
 static void *extend_heap(size_t words) {
+    printf("\n\nstarting to extend\n");
+
     char *bp;
     size_t size;
 
@@ -325,6 +397,8 @@ static void *extend_heap(size_t words) {
     PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); /* new epilogue header */
 
     /* Coalesce if the previous block was free */
+    printf("coalsece value is %p\n", coalesce(bp)); //delete line in final product 
+    printf("\n\nfinished exstending the heap\n");
     return coalesce(bp);
 }
 
@@ -354,6 +428,14 @@ static bool check_heap(int line) {
     }
 
     return true;
+}
+
+static void print_linked_list() {
+    printf("printing linked list:\n");
+    for (char *cur_block = (char*) NEXT_PTR(PSUB(heap_start, DSIZE)); PTR_NEXT_PTR(cur_block) != 0; cur_block = (char *) NEXT_PTR(cur_block)) {
+        // heap_start is set to the blk ptr of the prologue, so we do (heap_start-16) to get to the padding at the front
+        print_block(cur_block);
+    }
 }
 
 /*
